f922d82edf2b7bd94c409247a08c9ad73245d3f0
Luna <72428016+Lunas-Lab@users.noreply.github.com>
2026-06-10 21:15:51 +1200

    CreateShortcuts.bat updated with messages

--- CreateShortcuts.bat
+++ CreateShortcuts.bat
@@ -1,5 +1,10 @@
 @echo off
 
+echo "Creating Desktop and Start Menu shortcuts for Archivists Assistant"
+
 cd %~dp0
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%appdata%\Microsoft\Windows\Start Menu\Programs\Archivists Assistant.lnk');$s.TargetPath='%cd%\Archivists Assistant.bat'; $s.IconLocation='%cd%\The Archivists Assistant.ico'; $s.Save()"
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Desktop\Archivists Assistant.lnk');$s.TargetPath='%cd%\Archivists Assistant.bat'; $s.IconLocation='%cd%\The Archivists Assistant.ico'; $s.Save()"
+
+echo "Shortcuts created!"
+pause
